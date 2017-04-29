0dc353868ebceee0ef924613cc47a41f8a2d6537
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-04-29 12:03:37 +0200

    feat: first version of a simple http poller

--- notes.md
+++ notes.md
@@ -20,3 +20,18 @@ we would have bigger chances of receiving unusual HTTP responses,
 which will give us a biased result, if we are looking to test sheer performance.
 We keep the rest of the list to test, afterwards, the robustness
 of our poller.)
+
+we use the readline module so our test are compatible only
+with NodeJS 0.12 +
+arrow function : NodeJS 4.4.5
+
+note: our javascript is automatically formatted with prettier
+(with a vim pre-save hook) : https://github.com/prettier/prettier
+
+Remarks on http (80) polling method:
+- getting a response can be as high as 6s
+- connect ETIMEDOUT errors
+- read ETIMEDOUT errors
+- a lot of 301/302 redirects to https servers
+- some adresses not DNS resolved (getaddrinfo ENOTFOUND, which is
+surprising for the top 1000 websites)
---
+++ simplepoller.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const readline = require('readline');
+const fs = require('fs');
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream('top-1m.csv'),
+});
+
+const [firstLine = 1, lastLine] = process.argv.slice(2, 4).map(Number);
+
+let i = 1;
+lineReader.on('line', lineLog);
+
+function lineLog(line) {
+  if (i > firstLine - 1) {
+    setInterval(() => {
+      const url = line.split(',')[1];
+      const startDate = Date.now();
+      http
+        .get('http://' + url, res => {
+          console.log(url, res.statusCode, Date.now() - startDate);
+        })
+        .on('error', e => {
+          console.error(`Got error: ${e.message} ${url}`);
+        });
+    }, 1000);
+  }
+  i++;
+  if (i === lastLine + 1) lineReader.removeListener('line', lineLog);
+}
+
