1b5e639031f5b4970a918ec25815d44390f2608c
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-04-29 12:12:38 +0200

    fix: remove setInterval from poller

--- simplepoller.js
+++ simplepoller.js
@@ -13,17 +13,15 @@ lineReader.on('line', lineLog);
 
 function lineLog(line) {
   if (i > firstLine - 1) {
-    setInterval(() => {
-      const url = line.split(',')[1];
-      const startDate = Date.now();
-      http
-        .get('http://' + url, res => {
-          console.log(url, res.statusCode, Date.now() - startDate);
-        })
-        .on('error', e => {
-          console.error(`Got error: ${e.message} ${url}`);
-        });
-    }, 1000);
+    const url = line.split(',')[1];
+    const startDate = Date.now();
+    http
+      .get('http://' + url, res => {
+        console.log(url, res.statusCode, Date.now() - startDate);
+      })
+      .on('error', e => {
+        console.error(`Got error: ${e.message} ${url}`);
+      });
   }
   i++;
   if (i === lastLine + 1) lineReader.removeListener('line', lineLog);
