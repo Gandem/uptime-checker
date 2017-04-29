e6afe7c0c979e51e986a216c9951dd9ff81ca669
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-04-29 20:22:50 +0200

    chore: add comma-dangle rule to eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,14 @@ module.exports = {
     rules: {
         indent: [2, 4],
         'no-param-reassign': 0,
+        'comma-dangle': [
+            'error',
+            {
+                arrays: 'only-multiline',
+                objects: 'only-multiline',
+                functions: 'ignore',
+            },
+        ],
     },
 
     env: {
