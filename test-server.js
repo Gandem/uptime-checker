a4426b759011f72b07887abcdcb717308afb9d0d
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-05-05 22:52:24 +0200

    feat: simple http server for testing alert logic

---
+++ test-server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello');
+});
+
+server.listen(8080);
+
